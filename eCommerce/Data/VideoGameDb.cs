6444f1327b341806d33162aba5798540c50d4fa7
Jose Salgado <Josalgado03@gmail.com>
2019-08-20 18:13:43 -0700

    Implemented DB method to get all games

--- eCommerce/Data/VideoGameDb.cs
+++ eCommerce/Data/VideoGameDb.cs
@@ -1,4 +1,5 @@
 ﻿using eCommerce.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,26 @@ namespace eCommerce.Data
             await context.AddAsync(g);
             await context.SaveChangesAsync();
             return g;
+        }
+
+        /// <summary>
+        /// Retrieves all games sorted in alphabetical order by title.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns>A list of all games retrieved</returns>
+        public static async Task<List<VideoGame>> GetAllGames(GameContext context)
+        {
+            //LINQ Query Syntax
+            //List<VideoGame> games = await (from vidGame in context.VideoGames
+            //                         orderby vidGame.Title ascending
+            //                         select vidGame).ToListAsync();
+
+
+            //LINQ Method Syntax
+            List<VideoGame> games = await context.VideoGames
+                                    .OrderBy(g => g.Title)
+                                    .ToListAsync();
+            return games;
         }
     }
 }
